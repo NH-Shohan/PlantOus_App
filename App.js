b39a73470134070629323febf239ba34af618c96
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2022-12-08 00:54:04 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { color } from "./color";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View>
+          <Text style={styles.header}>Welcome</Text>
+          <Text style={styles.headerLogoName}>PlantOus</Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "flex-start",
+            marginTop: 10,
+            marginRight: 10,
+          }}
+        >
+          <AntDesign name="shoppingcart" size={30} color="black" />
+        </View>
+      </View>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
     </View>
   );
 }
@@ -13,8 +35,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 25,
+  },
+  headerLogoName: {
+    fontSize: 50,
+    color: color.green,
+    fontWeight: "600",
   },
 });
---
+++ color.js
@@ -0,0 +1,7 @@
+export const color = {
+    green: "#68AD32",
+    lightGreen: "DEFFC5",
+    light: "#F2F2F2",
+    white: "#FFFFFF",
+    black: "#000000"
+}
